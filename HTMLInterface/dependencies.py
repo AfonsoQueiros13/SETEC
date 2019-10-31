4508a4f4f8cda6c2d13da2f2ea58f573c0f7cad1
AfonsoQueiros13 <afonsoqueiros13@gmail.com>
2019-10-31 14:31:36 +0000

    update

--- HTMLInterface/dependencies.py
+++
@@ -1,8 +0,0 @@
-import scipy.io
-mat = scipy.io.loadmat('interfaces.mat')
-# print all info in interfaces.mat
-print(mat)
-# print only the 'simulation_data'
-print(mat['simulation_data'])
-# print only the 'measured_data'
-print(mat['measured_data'])
